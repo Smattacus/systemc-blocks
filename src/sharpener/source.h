1c571c918be17dd3f106ab8b8047b78a4f7d45e6
Smattacus <Smattacus@gmail.com>
2022-11-25 04:22:05 +0000

    Fix up source a bit.

--- src/sharpener/source.h
+++ src/sharpener/source.h
@@ -1,18 +1,20 @@
 #include "systemc.h"
 
-struct source: sc_module {
-    sc_in<bool> data_req;
-    sc_out<bool> data_valid;
-    sc_out<sc_int<8>> in_red;
-    sc_out<sc_int<8>> in_green;
-    sc_out<sc_int<8>> in_blue;
+class source: sc_module {
+    public:
+        sc_in<bool> data_req;
+        sc_out<bool> data_valid;
+        sc_out<sc_int<8>> in_red;
+        sc_out<sc_int<8>> in_green;
+        sc_out<sc_int<8>> in_blue;
 
-    sc_in_clk CLK;
+        sc_in_clk CLK;
 
-    SC_CTOR(source)
-    {
-        SC_CTHREAD(entry, CLK.pos());
-    }
+        SC_CTOR(source)
+        {
+            SC_CTHREAD(entry, CLK.pos());
+        }
 
-    void entry();
+        void entry();
+
 };
